02b50bcac2c5a9ba9d8a079260313bc9e2123fa4
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-20 12:58:09 +0530

    Added: stored result in dictionary and determined % of singlet combination.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
 
 echo "Welcome to Flip coin simulator."
-flipCoin=$((RANDOM%2))
-if [[ $flipCoin -eq 0 ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A dictFlip
+for (( i=0; i<10; i++ ))
+do
+	flipCoin=$((RANDOM%2))
+	if [[ $flipCoin -eq 0 ]]
+	then
+		dictFlip[heads]=$((${dictFlip[heads]}+1))
+	else
+		dictFlip[tails]=$((${dictFlip[tails]}+1))
+	fi
+done
+echo "${!dictFlip[@]} : ${dictFlip[@]}"
+
+headPercentage=$((${dictFlip[heads]}*100/i))
+tailPercentage=$((${dictFlip[tails]}*100/i))
