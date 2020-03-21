3265ca285f5827cf379909200892ae48bf047798
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-21 17:46:44 +0530

    Added:Triplet and percentage of it.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,56 +1,86 @@
 #!/bin/bash -x
+declare -A coinDict1
+declare -A coinDict2
+declare -A coinDict3
+declare -A coinArray1
+declare -A coinArray2
+declare -A coinArray3
 
-echo "Welcome to Flip coin simulator."
-declare -A dictSinglet
-read -p "Enter Singlet flip value: " singletFlips
-for (( i=0; i<$singletFlips; i++ ))
-do
-	coinside=""
-	randomSinglet=$((RANDOM%2))
-	if [[ $randomSinglet -eq 0 ]]
-	then
-		dictSinglet[H]=$((${dictSinglet[H]}+1))
-	else
-		dictSinglet[T]=$((${dictSinglet[T]}+1))
-	fi
-done
-echo "SingletKey :${!dictSinglet[@]} : SingletValue:${dictSinglet[@]}"
-headPercentage=$((${dictSinglet[H]}*100/i))
-tailPercentage=$((${dictSinglet[T]}*100/i))
-
-
-declare -A dictDoublet
-declare -A arrDoublet
-function percentage()
+function percentage1()
 {
-	local v=$1
-	coinPercent=$((${dictDoublet[$v]}*100/$doubletFlips))
-	arrDoublet[$v]=$coinPercent
+   local v=$1
+   coinPercent1=$((${coinDict1[$v]}*100/$no_of_flips))
+   coinArray1[$v]=$coinPercent1
+}
+
+function percentage2()
+{
+   local v=$1
+   coinPercent2=$((${coinDict2[$v]}*100/$no_of_flips))
+   coinArray2[$v]=$coinPercent2
+}
+
+function percentage3()
+{
+   local v=$1
+   coinPercent3=$((${coinDict3[$v]}*100/$no_of_flips))
+   coinArray3[$v]=$coinPercent3
 }
 
 function flipSimulator()
 {
-	for (( i=1; i<=$1; i++ ))
-	do
-		coinFlip=""
-		for (( j=1; j<=$2; j++ ))
-		do
-			randomCoin=$((RANDOM%2))
-			if [[ $randomCoin -eq 0 ]]
-			then
+   local coins=$1
+   local flips=$2
+   for (( i=1; i<=$flips; i++ ))
+   do
+      coinFlip=""
+      for (( j=1; j<=$coins; j++ ))
+      do
+         randomCoin=$((RANDOM%2+1))
+         if [[ $randomCoin -eq 1 ]]
+         then
 				coinFlip+="H"
-			else
-				coinFlip+="T"
-			fi
-		done
-		dictDoublet[$coinFlip]=$((${dictDoublet[$coinFlip]}+1))
-		percentage $coinFlip
-	done
+         elif [[ $randomCoin -eq 2 ]]
+			then
+            coinFlip+="T"
+         fi
+      done
+
+      if [[ $coins -eq $SINGLET ]]
+      then
+         coinDict1[$coinFlip]=$((${coinDict1[$coinFlip]}+1))
+         percentage1 $coinFlip
+      elif [[ $coins -eq $DOUBLET ]]
+      then
+         coinDict2[$coinFlip]=$((${coinDict2[$coinFlip]}+1))
+         percentage2 $coinFlip
+      else
+         coinDict3[$coinFlip]=$((${coinDict3[$coinFlip]}+1))
+         percentage3 $coinFlip
+      fi
+   done
 }
 
-echo "Doublet :"
+echo "Singlet flip coin"
+read -p "Number of flips: " no_of_flips
+SINGLET=1
+flipSimulator $SINGLET $no_of_flips
+
+echo "Key: ${!coinDict1[@]}   Value: ${coinDict1[@]}"
+echo "Key: ${!coinArray1[@]}  Percent: ${coinArray1[@]}"
+
+echo "Doublet flip coin "
+read -p "Number of flips: " no_of_flipsx
 DOUBLET=2
-read -p "Enter  flips for doublet: " doubletFlips
-flipSimulator $doubletFlips $DOUBLET
-echo "Key :${!dictDoublet[@]} Value:${dictDoublet[@]}"
-echo "Key:${!arrDoublet[@]}  Percentage: ${arrDoublet[@]}"
+flipSimulator $DOUBLET $no_of_flips
+
+echo "Key: ${!coinDict2[@]}   Value: ${coinDict2[@]}"
+echo "Key: ${!coinArray2[@]}  Percent: ${coinArray2[@]}"
+
+echo "Triplet flip coin"
+read -p "Number of Flips: " no_of_flips
+TRIPLET=3
+flipSimulator $TRIPLET $no_of_flips
+
+echo "Key: ${!coinDict3[@]}   Value: ${coinDict3[@]}"
+echo "Key: ${!coinArray3[@]}  Percent: ${coinArray3[@]}"
